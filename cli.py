859475f40e0ed4812caec7917bce3420061cdb5d
Jeff Do <jeff.do@bloomreach.com>
2016-05-30 21:41:01 -0700

    Add shorthand aliases for arguments

--- cli.py
+++ cli.py
@@ -11,8 +11,8 @@ def cli():
 
 @cli.command()
 @click.argument('s3path', type=str)
-@click.option('--recursive', is_flag=True, help="Recursively walk through files.")
-@click.option('--human', is_flag=True, help="Show filesize as human readable format.")
+@click.option('--recursive', '-r', is_flag=True, help="Recursively walk through files.")
+@click.option('--human', '-h', is_flag=True, help="Show filesize as human readable format.")
 def ls(s3path, recursive, human):
     """List files under s3path"""
     for file in s3lib.ls(s3path, recursive=recursive):
@@ -36,8 +36,8 @@ def ls(s3path, recursive, human):
 
 @cli.command()
 @click.argument('s3path', type=str)
-@click.option('--recursive', is_flag=True, help="Recursively walk through files.")
-@click.option('--human', is_flag=True, help="Show filesize as human readable format.")
+@click.option('--recursive', '-r', is_flag=True, help="Recursively walk through files.")
+@click.option('--human', '-h', is_flag=True, help="Show filesize as human readable format.")
 def du(s3path, recursive, human):
     """List disk usage (total file size) under s3path"""
     total_bytes = s3lib.du(s3path, recursive=recursive)
