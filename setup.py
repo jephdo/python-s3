71221d952e38d672271be08716b4080b32fc2e2e
Jeff Do <jeff.do@bloomreach.com>
2016-05-30 21:41:27 -0700

    Add requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 
 setup(
     name='s3lib',
     version='0.1',
     packages=['s3lib'],
     py_modules=['cli'],
+    install_requires=required,
     entry_points="""
         [console_scripts]
         s3lib=cli:cli
